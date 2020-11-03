e6a568c100e85495d37673ddfa7d632af7de873b
jmsmrgn <james@morg.dev>
2020-11-02 17:15:20 -0800

    init rich text field

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Paragraph } from '@contentful/forma-36-react-components';
+import React, { useEffect } from 'react';
 import { FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import { RichTextEditor } from '@contentful/field-editor-rich-text';
 
 interface FieldProps {
   sdk: FieldExtensionSDK;
 }
 
 const Field = (props: FieldProps) => {
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
-  return <Paragraph>Hello Entry Field Component</Paragraph>;
+  useEffect(() => {
+    props.sdk.window.startAutoResizer();
+  }, []);
+
+  return <RichTextEditor sdk={props.sdk} />
 };
 
 export default Field;
